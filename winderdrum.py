25ddcf68e4a1f8688573e88a1f4baa6ef5b2ee7b
Robert Markoski <robche@gmail.com>
2011-05-10 13:24:56 +0800

    Modified code for data to be stored in table. Formatted Output

--- winderdrum.py
+++ winderdrum.py
@@ -1,5 +1,6 @@
 #Enter Drum
 import math
+import sys
 """
 drumw = int(input("Enter Drum Width (mm): "))
 drumd = int(input("Enter Drum Diameter (mm): "))
@@ -16,8 +17,23 @@ ropel = 500 #Length of Rope
 roped = 24 #Diameter of Rope
 grooved = "Y" #Is drum grooved?
 
-def printoutput(layer, length, pcd, coils, cuml):
-	print("| {:d} | {:.2f} | {:.2f} | {:.2f} | {:.2f} |".format(layer,length,pcd,coils,cuml))
+def get_max_width(table1,index1):
+    """Get max width given column index"""
+    return max([len(format(row1[index1])) for row1 in table])
+
+
+
+def printoutput(tdata):
+    colpad = []
+    out = sys.stdout
+    for i in range(len(tdata[0])):
+        colpad.append(get_max_width(table,i))
+    for row in tdata:
+        for i in range(0,len(row)):
+            col = format(row[i]).center(colpad[i]+1)
+            print(col, end=" | ",file=out)
+        print(file=out)
+    return
 
 length = 0
 cuml = 0
@@ -25,40 +41,40 @@ flangd = 0
 pcd = 0
 layer = 1 
 
-
 roped2 = roped * 1.02 #Include for rope stretch.
 layerb = round(drumw/roped2) #Estimate amount of layers on bottom layer of drum
 aroped = drumw/layerb #average diameter of rope
 coils = round(drumw/aroped)
 delta = (aroped**2- aroped**2/4)**0.5
-
+table = [["Layer","Length (m)","PCD (mm)","Coils","Cumulative Length (m)"]]
 print("delta: {: .2f}".format(delta))
-print("| Layer | Length | PCD | Coils | Cumulative Length |")
-while (cuml < ropel):
+
+while (cuml < ropel):	
     if (layer == 1):
-        table = ""
-        pcd = drumd + aroped
-        length = pcd/1000 * 3.14159 * coils
+        pcd = round(drumd + aroped,2)
+        length = round(pcd/1000 * 3.14159 * coils,2)
+
         cuml = length
-        printoutput(layer,length,pcd,coils,cuml)
+        table.append([layer,length,pcd,coils,cuml])
         layer = 2
-        
+    
     else:
-        pcd = pcd + delta
+        pcd = round(pcd + delta,2)
         if (layer % 2):
             coils = coils -1
         else:
             coils = coils +1
-        length = pcd/1000 * 3.14159 * coils
-        cuml = cuml + length
+        length = round(pcd/1000 * 3.14159 * coils,2)
+        cuml = round(cuml + length,2)
         if (cuml > ropel):
-            ropeleft = ropel - (cuml - length)
-            coils = round(ropeleft / (pcd/1000 * 3.14159))
-            cuml = ropel
-        print("{:d} | {:.2f} | {:.2f} | {:.2f} | {:.2f}".format(layer,length,pcd,coils,cuml))
-        layer = layer +1
+            ropeleft = round(ropel - (cuml - length),2)
+            coils = round(ropeleft / (pcd/1000 * 3.14159),0)
+            cuml = round(ropel,2)
         
+        table.append([layer,length,pcd,coils,cuml])
+        layer = layer +1
 
+printoutput(table)
 
 
     
